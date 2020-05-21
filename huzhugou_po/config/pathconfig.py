bd0f5a7f429a43b521366c99737f8292180cdbbc
duxiuyan <duxiuyan@126.com>
2020-05-21 10:33:33 +0800

    配置测试数据的路径

--- huzhugou_po/config/pathconfig.py
+++ huzhugou_po/config/pathconfig.py
@@ -3,6 +3,10 @@ import os
 # 项目根路径
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#获取测试数据文件所在路径
-CONFIG_DIR = os.path.join(BASE_DIR,"config")
-USERCONFIG_FILE_PATH = os.path.join(CONFIG_DIR,"userconfig.yaml")
+# 获取购物主流程的测试数据文件所在路径
+CONFIG_DIR = os.path.join(BASE_DIR, "testcase/data")
+MAIN_SHOPPING_FLOW_DATA_PATH = os.path.join(CONFIG_DIR, "main_shopping_flow_data.yaml")
+
+# 获取我的->收货地址->新增收货地址测试数据文件所在路径
+TESTCASE_DATA_DIR = os.path.join(BASE_DIR, "testcase/data")
+MY_ADDRESS_DATA_PATH = os.path.join(TESTCASE_DATA_DIR, "my_address_data.yaml")
