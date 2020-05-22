828654651b8402acb06955956739fec759438a59
duxiuyan <duxiuyan@126.com>
2020-05-22 12:33:13 +0800

    更新调用的前置函数名

--- huzhugou_po/testcase/my/test_add_address.py
+++ huzhugou_po/testcase/my/test_add_address.py
@@ -6,15 +6,16 @@ from huzhugou_po.page.huzhugou_page import HuZhuGouPage
 from appium import webdriver
 
 
-@pytest.mark.usefixtures("init_driveranddopre")
+@pytest.mark.usefixtures("init_driver")
 class TestAddAddress:
 
-    #执行用例：新增收货地址，并断言新增的收货人是否在收货地址列表中
     @pytest.mark.parametrize("consigneedata", yaml.safe_load(open(ADD_ADDRESS_DATA_PATH)))
-    def test_add_address(self, init_driveranddopre, consigneedata):
+    def test_add_address(self, init_driver, consigneedata):
+        '''执行用例：新增收货地址，并断言新增的收货人是否在收货地址列表中'''
         consignee = consigneedata["consignee"]  # 获取收货人测试数据
         consigneephone = consigneedata["consigneephone"]  # 获取收货人联系方式测试数据
         consigneeaddress = consigneedata["consigneeaddress"]  # 获取收货人地址测试数据
         consigneedetailsaddress = consigneedata["consigneedetailsaddress"]  # 获取收货人详细地址测试数据
-        assert HuZhuGouPage.goto_my_add_address(init_driveranddopre).add_address(consignee, consigneephone, consigneeaddress,
-                                                                     consigneedetailsaddress) == True
+        assert HuZhuGouPage.goto_my_add_address(init_driver).add_address(consignee, consigneephone,
+                                                                                 consigneeaddress,
+                                                                                 consigneedetailsaddress) == True
