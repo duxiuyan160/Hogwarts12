d74732d1b8d1a67dbe32150e1923a5c6da6a220c
duxiuyan <duxiuyan@126.com>
2020-05-22 12:23:45 +0800

    将实例化driver和弹框处理、短信登录的操作分开

--- huzhugou_po/conftest.py
+++ huzhugou_po/conftest.py
@@ -1,5 +1,7 @@
 '''
-前置函数，在测试每个用例页时，都需要实例化driver、点击条款中的同意按钮和验证码登录
+前置函数：
+1、init_driver：只实例化webdriver
+2、init_driveranddopre：第一个测试的测试用例需要调用此函数,后面的操作不要在会话前重置应用状态
 '''
 import pytest
 from selenium.webdriver.common.by import By
@@ -9,7 +11,14 @@ from huzhugou_po.page.huzhugou_page import HuZhuGouPage
 
 
 @pytest.fixture("class")
+def init_driver():
+    '''只实例化webdriver'''
+    driver = HuZhuGouPage()  # 实例化主向导页面，包含跳转到各个模块和初始化driver
+    yield driver  # 返回driver
+
+@pytest.fixture("class")
 def init_driveranddopre():
+    '''实例化webdriver,第一个测试的用例中调用此函数'''
     driver = HuZhuGouPage()  # 实例化主向导页面，包含跳转到各个模块和初始化driver
     driver.click_agree()  # 点击条款中的同意按钮
     driver.login_by_code()  # 短信验证码登录
