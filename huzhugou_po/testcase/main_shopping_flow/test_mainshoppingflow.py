7d9be3639ed93cb4a0ec877c5f4bc8452d753c5a
duxiuyan <duxiuyan@126.com>
2020-05-22 12:40:14 +0800

    更改了调用的前置函数名（配合onReset=True）

--- huzhugou_po/testcase/main_shopping_flow/test_mainshoppingflow.py
+++ huzhugou_po/testcase/main_shopping_flow/test_mainshoppingflow.py
@@ -4,9 +4,9 @@ from huzhugou_po.page.huzhugou_page import HuZhuGouPage
 from appium import webdriver
 
 
-@pytest.mark.usefixtures("init_driveranddopre")
+@pytest.mark.usefixtures("init_driver")
 class TestMainShoppingFlow:
 
     # 执行用例：验证码登录->首页搜索商品->商品展示->商品加入购物车->结算->提交订单，并断言
-    def test_mainshoppingflow(self, init_driveranddopre):
-        assert "支付剩余时间" in HuZhuGouPage.goto_mainshoppingflow(init_driveranddopre).MainShoppingFlow()
+    def test_mainshoppingflow(self, init_driver):
+        assert "支付剩余时间" in HuZhuGouPage.goto_mainshoppingflow(init_driver).MainShoppingFlow()
